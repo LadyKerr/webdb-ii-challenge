6880facb24c3837354a5d59e49499853f6724406
LadyKerr <kadiekerr@gmail.com>
2019-07-16 18:45:40 -0400

    UPDATE & DELETE

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -22,7 +22,38 @@ router.post("/", async (req, res) => {
     const addNewCar = await carsDb("cars").where({ id });
     res.status(201).json(addNewCar);
   } catch (err) {
+    console.log(err);
     res.status(500).json({ message: "Failed to add car to the database." });
+  }
+});
+
+//UPDATE
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const updatedCar = await carsDb("cars")
+      .where({ id: id })
+      .update(req.body);
+    res
+      .status(200)
+      .json({ message: `${updatedCar} record(s) has been updated.` });
+  } catch (err) {
+    res.status(500).json({ message: "Could not update file." });
+  }
+});
+
+//DELETE
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedCar = await carsDb("cars")
+      .where({ id: id })
+      .del();
+    res.status(200).json({ message: `${deletedCar} record(s) was deleted.` });
+  } catch (err) {
+    res.status(500).json({ message: "Could not update file." });
   }
 });
 
