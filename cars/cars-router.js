da3bf51b4a893ddee00c5bbb29c58e8b1283feb4
LadyKerr <kadiekerr@gmail.com>
2019-07-16 17:06:39 -0400

    GET & POST requests and knex config setUp

---
+++ cars/cars-router.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const carsDb = require("../data/db-config.js");
+
+const router = express.Router();
+
+//Read
+router.get("/", async (req, res) => {
+  try {
+    const cars = await carsDB("cars");
+    res.status(200).json(cars);
+  } catch (err) {
+    res.status(500).json({ message: "Failed to get cars from database." });
+  }
+});
+
+//Create
+router.post("/", async (req, res) => {
+  try {
+    const newCar = req.body;
+    const [id] = await carsDb("cars").insert(newCar);
+    const addNewCar = await carsDb("cars").where({ id });
+    res.status(201).json(addNewCar);
+  } catch (err) {
+    res.status(500).json({ message: "Failed to add car to the database." });
+  }
+});
+
+module.exports = router;
