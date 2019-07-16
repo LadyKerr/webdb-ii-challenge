052f6a4c79212c32ee3ecac1b35918a21a98de41
LadyKerr <kadiekerr@gmail.com>
2019-07-16 18:04:46 -0400

    seeded data

--- data/seeds/001-cars.js
+++ data/seeds/001-cars.js
@@ -1,13 +1,34 @@
-
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return knex("cars")
+    .truncate()
+    .then(function() {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex("cars").insert([
+        {
+          VIN: "HH5256HH2HGFH236",
+          make: "Honda",
+          model: "Civic",
+          mileage: 105000,
+          "transmission type": "unknown",
+          "title status": "pending"
+        },
+        {
+          VIN: "GFT525JUHJJI5552",
+          make: "Nissan",
+          model: "Accord",
+          mileage: 15000,
+          "transmission type": "V6",
+          "title status": "own"
+        },
+        {
+          VIN: "WW56852FF52482",
+          make: "Mazda",
+          model: "HapTown",
+          mileage: 120000,
+          "transmission type": "V12",
+          "title status": "pending"
+        }
       ]);
     });
 };
